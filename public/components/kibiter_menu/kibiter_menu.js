140c9398cc0294eb09df357ca4780fb48ec71c0e
David Moreno <dmoreno@bitergia.com>
2020-10-27 10:39:53 +0100

    Add functionality to JQuery
    Signed-off-by: David Moreno <dmoreno@bitergia.com>

--- public/components/kibiter_menu/kibiter_menu.js
+++ public/components/kibiter_menu/kibiter_menu.js
@@ -15,7 +15,7 @@
 
 /* eslint-disable no-restricted-globals */
 import $ from 'jquery';
-import { getKibiterMenu } from './kibiter_menu_ui';
+import { getKibiterMenu, getKibiterJSMenu, fillDropdownItems, redirectToPanel } from './kibiter_menu_ui';
 import angular from 'angular';
 import { renderKibiterMenu } from './render_kibiter_menu'
 import './kibiter_menu_style.scss';
@@ -44,28 +44,72 @@ async function getMetadashboard() {
   return json
 }
 
-const highlightMenu = (scope, metadash) => {
-  scope.$root.selectedItem = undefined
+function getCurrentDashboard(metadash) {
+  let currentParent = undefined;
   // Check if you are in dashboard section
   if (window.location.href.split("dashboards#/view/")[1]) {
-      let currentDash = window.location.href.split("dashboards#/view/")[1].split('?')[0]
-      metadash.some((item) => {
-          if (item.type === "entry" && item.panel_id === currentDash) {
-              scope.$root.selectedItem = item
-          } else if (item.type === "menu") {
-              item.dashboards.some((subitem) => {
-                  if (subitem.type === "entry" && subitem.panel_id === currentDash) {
-                      scope.$root.selectedItem = item
-                      return true
-                  }
-              })
+    let currentDash = window.location.href.split("dashboards#/view/")[1].split('?')[0]
+    metadash.some((item) => {
+      if (item.type === "entry" && item.panel_id === currentDash) {
+        currentParent = item;
+      } else if (item.type === "menu") {
+        item.dashboards.some((subitem) => {
+          if (subitem.type === "entry" && subitem.panel_id === currentDash) {
+            currentParent = item
+            return true
           }
-          if (scope.$root.selectedItem) {
-              return true
-          }
-      });
+        })
+      }
+      if (currentParent) {
+        return true
+      }
+    });
   }
+  return currentParent
 }
+
+
+function chunkify(a, n, balanced) {
+
+  if (n < 2)
+    return [a];
+
+  let len = a.length,
+    out = [],
+    i = 0,
+    size;
+
+  if (len % n === 0) {
+    size = Math.floor(len / n);
+    while (i < len) {
+      out.push(a.slice(i, i += size));
+    }
+  }
+
+  else if (balanced) {
+    while (i < len) {
+      size = Math.ceil((len - i) / n--);
+      out.push(a.slice(i, i += size));
+    }
+  }
+
+  else {
+
+    n--;
+    size = Math.floor(len / n);
+    if (len % size === 0)
+      size--;
+    while (i < size * n) {
+      out.push(a.slice(i, i += size));
+    }
+    out.push(a.slice(size * n));
+
+  }
+
+  return out;
+}
+// var alphabet=['a','b','c','d','e','f'];
+// var alphabetPairs=splitArrayIntoChunksOfLen(alphabet,2);
 
 async function locationHashChanged() {
 
@@ -80,25 +124,81 @@ async function locationHashChanged() {
 
     if (navBarMenu && navBarMenu.length) {
       try {
-        if ($('#kibitermenu').length) {
+        if ($('#kibiterjsmenu').length) {
           return;
         }
-        const kibiterMenuItem = document.createElement('div');
-        kibiterMenuItem.setAttribute("id", "kibitermenu");
-        let compile = angular.injector(['ng']).get('$compile')
-        let scope = angular.element(navBarMenu[0]).scope()
-        //angular.injector(['ng']).get('$rootScope')
-        navBarMenu[0].insertBefore(kibiterMenuItem, navBarMenu[0].firstChild)
-        kibiterMenuItem.innerHTML = getKibiterMenu();
-        compile(kibiterMenuItem)(scope);
-        renderKibiterMenu(scope.$root)
-        scope.$root.appTitleCustom = projectname.data.projectname.name
-        scope.$root.showKibiterMenu = false
-        scope.$root.metadash = metadashboard.data.metadashboard
-        highlightMenu(scope, scope.$root.metadash)
-        // await getProjectName().then(function (resp) {
-        //   scope.$root.appTitleCustom = resp
-        // })
+        // const kibiterMenuItem = document.createElement('div');
+        // kibiterMenuItem.setAttribute("id", "kibitermenu");
+        // let compile = angular.injector(['ng']).get('$compile')
+        // let scope = angular.element(navBarMenu[0]).scope()
+        // //angular.injector(['ng']).get('$rootScope')
+        // //navBarMenu[0].insertBefore(kibiterMenuItem, navBarMenu[0].firstChild)
+        // kibiterMenuItem.innerHTML = getKibiterMenu();
+        // compile(kibiterMenuItem)(scope);
+        // renderKibiterMenu(scope.$root)
+        // scope.$root.appTitleCustom = projectname.data.projectname.name
+        // scope.$root.showKibiterMenu = false
+        // scope.$root.metadash = metadashboard.data.metadashboard
+        // highlightMenu(scope, scope.$root.metadash)
+        // // await getProjectName().then(function (resp) {
+        // //   scope.$root.appTitleCustom = resp
+        // // })
+
+        const kibiterJSMenuItem = document.createElement('div');
+        kibiterJSMenuItem.setAttribute("id", "kibiterjsmenu");
+        let currentDashboard = getCurrentDashboard(metadashboard.data.metadashboard)
+        kibiterJSMenuItem.innerHTML = getKibiterJSMenu($, projectname.data.projectname.name, metadashboard.data.metadashboard, 4, currentDashboard);
+        navBarMenu[0].insertBefore(kibiterJSMenuItem, navBarMenu[0].firstChild)
+
+
+        ///////////////////////////////
+        // Redirect to overview when clicking on project name
+        $('#kibiterProjectName').click(function () {
+          redirectToPanel('', 'Overview')
+        });
+
+        // Click feature
+        metadashboard.data.metadashboard.forEach(item => {
+          $('#kibiterMenuItem' + item.name.replace(/[^a-zA-Z0-9]+/g, '')).click(function () {
+            if (item.type === "entry") {
+              redirectToPanel(item.name, item.panel_id)
+            } else if (item.type === "menu") {
+              let currentPanel = window.location.href.split("/dashboards#/view/")[1].split("?")[0]
+              if ($("#kibiterDropdownTitle0").text() === item.name) {
+                $("#kibiterItemDropdown").hide()
+                $("#kibiterDropdownTitle0").text("")
+                $("#kibiterDrowpdownItems0").html("")
+              } else {
+                $("#kibiterItemDropdown").show()
+                $("#kibiterDropdownTitle0").text(item.name)
+                let groups = chunkify(item.dashboards, 4, true)
+                for (let i = 0; i < groups.length; i++) {
+                  $("#kibiterDrowpdownItems" + i).html(fillDropdownItems(item, groups[i]))
+                }
+              }
+              console.log(item)
+            }
+          });
+
+          // Logic click for sons
+          if (item.type === "menu") {
+            item.dashboards.forEach(subitem => {
+              // Redirect to panel
+              $(document).on('click', '#kibiterDropdownItem' + item.name.replace(/[^a-zA-Z0-9]+/g, '') + subitem.panel_id.replace(/[^a-zA-Z0-9]+/g, ''), function () {
+                redirectToPanel(subitem.name, subitem.panel_id)
+              });
+              $(document).on('mouseenter', '#kibiterDropdownItem' + item.name.replace(/[^a-zA-Z0-9]+/g, '') + subitem.panel_id.replace(/[^a-zA-Z0-9]+/g, ''), function () {
+                $("#kibiterItemDropdownDescription").html(subitem.description)
+                $("#kibiterItemDropdownTitle").html(subitem.title)
+              });
+              $(document).on('mouseleave', '#kibiterDropdownItem' + item.name.replace(/[^a-zA-Z0-9]+/g, '') + subitem.panel_id.replace(/[^a-zA-Z0-9]+/g, ''), function () {
+                $("#kibiterItemDropdownDescription").html("")
+                $("#kibiterItemDropdownTitle").html("")
+              });
+            })
+          }
+
+        });
       } catch (e) {
         return e
       } finally {
@@ -115,25 +215,25 @@ async function locationHashChanged() {
   });
 }
 
-history.pushState = ( f => function pushState(){
+history.pushState = (f => function pushState() {
   var ret = f.apply(this, arguments);
   window.dispatchEvent(new Event('pushstate'));
   window.dispatchEvent(new Event('locationchange'));
   return ret;
 })(history.pushState);
 
-history.replaceState = ( f => function replaceState(){
+history.replaceState = (f => function replaceState() {
   var ret = f.apply(this, arguments);
   window.dispatchEvent(new Event('replacestate'));
   window.dispatchEvent(new Event('locationchange'));
   return ret;
 })(history.replaceState);
 
-window.addEventListener('popstate',()=>{
+window.addEventListener('popstate', () => {
   window.dispatchEvent(new Event('locationchange'))
 });
 
-window.addEventListener('locationchange', function(){
+window.addEventListener('locationchange', function () {
   locationHashChanged();
 })
 
--- public/components/kibiter_menu/kibiter_menu_ui.js
+++ public/components/kibiter_menu/kibiter_menu_ui.js
@@ -67,3 +67,87 @@ export const getKibiterMenu = () => {
 </div>
   `
 }
+
+export const getKibiterJSMenu = ($, projectname, items, columns, currentParent) => {
+  let menu = `
+<div class="kuiLocalNavRow theme-dark kibiterLocalNav">
+  <div class="kuiLocalNavRow__section">
+    <div class="kuiLocalBreadcrumbs">
+      <div class="kuiLocalBreadcrumb">
+        <a class="kuiLocalBreadcrumb__link projectName" id="kibiterProjectName">
+          ` + projectname + `
+        </a>
+      </div>
+
+    </div>
+  </div>
+  <div class="kuiLocalNavRow__section">
+    <div class="kuiLocalMenu">`
+
+
+
+  items.forEach(item => {
+    menu += `<button class="" id="kibiterMenuItem` + item.name.replace(/[^a-zA-Z0-9]+/g, '') + `">
+           <div class="kuiLocalMenuItem kibiterLocalMenuItem`
+    if (currentParent === item) {
+      menu += ` selected-kibiter-item`
+    }
+    menu += `">
+             ` + item.name + `
+           </div>
+         </button>`
+  });
+  menu += `    </div>
+  </div>
+</div>
+<div class="kuiLocalDropdown kibiterLocalDropdown" id="kibiterItemDropdown" style="display: none">
+  <!-- Dropdown close button -->
+  <div className="kuiLocalDropdownPanels">
+    <div class="col-md-4" style="margin-top: 20px">
+      <div class="kibiterLocalDropdownTitle" id="kibiterItemDropdownTitle"></div>
+
+      <div class="kibiterLocalDropdownText" id="kibiterItemDropdownDescription">
+      </div>
+    </div>`
+
+
+  for (let i = 0; i < columns; i++) {
+    menu += `<div class="col-md-2">
+    <div class="kibiterLocalDropdownTitle" id="kibiterDropdownTitle` + i + `"></div>
+    <div>
+      <ul class="list-unstyled" id="kibiterDrowpdownItems` + i + `">
+
+      </ul>
+    </div>
+    </div>`
+  }
+
+  menu += `
+  </div>
+</div>
+  `
+
+  return menu
+}
+
+export const redirectToPanel = (name, panel_id) => {
+  $("#kibiterItemDropdown").hide()
+  if (name === "Contact") {
+    window.location.replace(panel_id)
+  } else {
+    window.location.replace(window.location.href.split("app/")[0] + "app/dashboards#/view/" + panel_id)
+  }
+}
+
+export const fillDropdownItems = (parent, dashboards) => {
+  let menuPart = ""
+  dashboards.forEach(item => {
+    menuPart += `
+      <li>
+      <a id="kibiterDropdownItem`+ parent.name.replace(/[^a-zA-Z0-9]+/g, '') + item.panel_id.replace(/[^a-zA-Z0-9]+/g, '') + `" ng-mouseover="$root.showDescription(subitem)" ng-mouseleave="$root.hideDescription()" class="kuiLink submenuitem"
+        tabindex="0" role="button">` + item.name + `</a>
+    </li>`
+
+  });
+  return menuPart
+}
