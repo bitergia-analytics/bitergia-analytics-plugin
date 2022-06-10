ee213c56e1bec4cb505c535d40a3bfc5721ccce9
Eva Millán <evamillan@bitergia.com>
2022-06-10 17:25:52 +0200

    Setup test configuration
    Sets up Jest, using and extending OpenSearch
    Dashboards' own configuration.
    
    Signed-off-by: Eva Millán <evamillan@bitergia.com>

---
+++ test/mocks.ts
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2021-2022 Bitergia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import { coreMock } from '../../../src/core/public/mocks';
+
+const metadashboardMock = {
+  metadashboard: [
+    {
+      name: 'Overview',
+      dashboard_id: 'Overview',
+      type: 'entry',
+    },
+    {
+      name: 'About',
+      type: 'menu',
+      dashboards: [
+        {
+          name: 'About',
+          type: 'entry',
+          dashboard_id: 'About',
+        },
+        {
+          name: 'Contact',
+          type: 'entry',
+          dashboard_id: 'http://contact-url.com',
+        },
+      ],
+    },
+  ]
+};
+
+const initializerContextMock = {
+  config: {
+    get: jest.fn().mockReturnValue({
+      branding: {
+        projectName: 'Mock name',
+      },
+    }),
+  },
+};
+
+const coreServicesMock = {
+  application: {
+    getUrlForApp: jest.fn(),
+  },
+  chrome: {
+    navControls: {
+      registerCenter: jest.fn(),
+    },
+    setBreadcrumbs: jest.fn(),
+  },
+  http: {
+    fetch: jest.fn().mockResolvedValue({
+      data: metadashboardMock,
+    }),
+    put: jest.fn().mockResolvedValue({
+      hits: [
+        {
+          _source: {
+            dashboard: {
+              title: 'Dashboard title',
+            },
+          },
+          _id: '12345',
+        },
+      ],
+    }),
+  },
+};
+
+const mountParamsMock = coreMock.createAppMountParamters('/');
+
+const historyMock = {
+  listen: jest.fn(),
+  location: {
+    hash: '#/view/About',
+  },
+};
+
+export default {
+  metadashboardMock,
+  initializerContextMock,
+  coreServicesMock,
+  mountParamsMock,
+  historyMock,
+};
---
+++ test/run_jest_tests.js
@@ -0,0 +1,4 @@
+process.argv.push('--runInBand');
+
+require('../../../src/setup_node_env');
+require('../../../src/dev/jest/cli');
